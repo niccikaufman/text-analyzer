ce04d308dee7c000f0de5ae1f94a261c8267d13a
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-09 20:32:51 -0700

    add bolded passage function and utility logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
-//business logic
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
+// Business Logic
+
 function wordCounter(text) {
-  if(text.trim().length === 0) {
+  if (text.trim().length === 0) {
     return 0;
   }
   let wordCount = 0;
   const wordArray = text.split(" ");
-  wordArray.forEach(function(element){
+  wordArray.forEach(function(element) {
     if (!Number(element)) {
       wordCount++;
     }
@@ -14,28 +21,49 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
-  let wordCount = 0
+  let wordCount = 0;
   wordArray.forEach(function(element) {
-  if (element.toLowerCase().includes(word.toLowerCase())) {
-    wordCount++
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
     }
   });
   return wordCount;
 }
 
-// UI logic
-$(document).ready(function() {
-  $('form#word-counter').submit(function(event) {
+// UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
     event.preventDefault();
-    const passage = $('#text-passage').val();
-    const word = $('#word').val();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
     const wordCount = wordCounter(passage);
-    const occurrencesOfWord = numberOfOccurrencesInText(word,passage);
-    $('#total-count').html(wordCount);
-    $('#selected-count').html(occurrencesOfWord);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
